fbe5a370caf531fbeddd06f84c02c844f80f6612
Anjan shakya <anjansh@metropolia.fi>
2022-12-10 00:30:55 +0200

    localhost3000/product is working, changed backend to product and  static express to just productRouter and without using express.static. Also now we run by nodemon and not start app.js

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const productRouter = require("./backend/router/productRouter");
 
 const port = 3000;
 
-app.use("/backend", express.static("productRouter"));
+app.use('/product', productRouter);
 
-app.listen(port, () => console.log(`Port is listenoing ${port}`));
+app.listen(port, () => console.log(`Port is listening ${port}`));
